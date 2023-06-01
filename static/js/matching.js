8bcd11a8ec8214324f8dbd50dcecf0498bc8efc9
danielchandg <dawichan@ucsc.edu>
2023-06-01 14:48:41 -0700

    Small bug

--- static/js/matching.js
+++ static/js/matching.js
@@ -31,7 +31,7 @@ let init = function (app) {
     add_match_mode: false,
     search_class: '',
     search_professor: '',
-    hovered_class_term: -1,
+    hovered_class_term: {},
     update_dropdown_menu: 0,
     dropdown_hover: false,
   };
